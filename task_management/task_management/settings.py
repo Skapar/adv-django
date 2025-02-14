704f2ce798921e648459522f6aae39b7ff31b0ce
Your New Username <yournewemail@example.com>
2025-02-14 06:44:55 +0500

    extension

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
